c274a4016643c9c8a5decf014ed57f683468b879
Pao <velascop@gmail.com>
2017-10-17 23:58:23 -0600

    Removing about

--- src/components/shared/Header.js
+++ src/components/shared/Header.js
@@ -1,12 +1,10 @@
 import React, {PropTypes} from 'react';
-import {Link, IndexLink} from 'react-router';
+import {IndexLink} from 'react-router';
 
 const Header = () => {
   return (
     <nav>
       <IndexLink to="/" activeClassName="active">Home</IndexLink>
-      {" | "}
-      <IndexLink to="/about" activeClassName="active">About</IndexLink>
     </nav>
   );
 };
--- src/routes.js
+++ src/routes.js
@@ -2,14 +2,12 @@ import React from 'react';
 import {Route, IndexRoute} from 'react-router';
 import App from './components/App';
 import HomePage from './components/home/HomePage';
-import AboutPage from './components/about/AboutPage';
 import Problem1 from './components/problems/Problem1';
 import Problem7 from './components/problems/Problem7';
 
 export default(
   <Route path="/" component={App}>
     <IndexRoute component={HomePage} />
-    <Route path="about" component={AboutPage} />
     <Route path="1" component={Problem1} />
     <Route path="7" component={Problem7} />
   </Route>
